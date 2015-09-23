362f5b6d0fc67422683fd9c085882604c6e4bb89
Senica Gonzalez <senica@allebrum.com>
2015-09-22 20:03:58 -0700

    Add final filename to the client details object.

--- server.js
+++ server.js
@@ -68,6 +68,7 @@ function SocketIOFileUploadServer() {
 	 */
 	var _emitComplete = function (socket, id, success) {
 		var fileInfo = files[id];
+		fileInfo.clientDetail.finalName = fileInfo.pathName.split('/').pop();
 		socket.emit("siofu_complete", {
 			id: id,
 			success: success,
