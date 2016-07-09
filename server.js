f18863b8cedcd365a604d445cb0168474366eb3d
vote539 <vote539@gmail.com>
2016-07-09 13:53:02 -0700

    Revert "Add final filename to the client details object."

--- server.js
+++ server.js
@@ -76,7 +76,6 @@ function SocketIOFileUploadServer() {
 	 */
 	var _emitComplete = function (socket, id, success) {
 		var fileInfo = files[id];
-		fileInfo.clientDetail.finalName = path.basename(fileInfo.pathName);
 		socket.emit("siofu_complete", {
 			id: id,
 			success: success,
