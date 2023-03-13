1db59bb0125117d78c91212cb4fa8f707edb5012
Dymerz <21324342+Dymerz@users.noreply.github.com>
2023-03-13 20:22:39 +0100

    Fix bad socket.io in client

--- client.d.ts
+++ client.d.ts
@@ -1,7 +1,6 @@
-// External modules
 import { Router }      from 'express';
 import { WriteStream } from 'fs';
-import { Socket }      from 'socket.io';
+import { Socket }      from 'socket.io-client';
 
 export type SocketIOFileUploadClientOptions =
 {
